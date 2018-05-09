f821470e2747bb0698be5e77d13758c690ac613e
Gorka Maiztegi Etxeberria <gmaiztegi@gmail.com>
2018-05-09 11:00:26 +0200

    Take into account the day plus one in tests

--- spec/App/Util/ConsignmentFinderSpec.php
+++ spec/App/Util/ConsignmentFinderSpec.php
@@ -21,11 +21,15 @@ class ConsignmentFinderSpec extends ObjectBehavior
     private $amount = 10.00;
     private $index = 'the_index';
     private $consignment = 'the_consignment';
+    private $date;
+    private $datePlusOne;
 
-    function let(FinderKeyCreator $finderKeyCreator, \DateTime $date)
+    function let(FinderKeyCreator $finderKeyCreator)
     {
-        $date->beConstructedWith(array('2017-01-01'));
-        $finderKeyCreator->createIndex($date, $this->lastDigits, $this->amount)->willReturn($this->index);
+        $this->date = new \DateTime('2017-01-01');
+        $this->datePlusOne = new \DateTime('2017-01-02');
+        $finderKeyCreator->createIndex($this->date, $this->lastDigits, $this->amount)->willReturn($this->index);
+        $finderKeyCreator->createIndex($this->datePlusOne, $this->lastDigits, $this->amount)->willReturn($this->index);
         $this->beConstructedWith(array($this->index => $this->consignment), $finderKeyCreator);
     }
 
@@ -34,24 +38,26 @@ class ConsignmentFinderSpec extends ObjectBehavior
         $this->shouldHaveType(ConsignmentFinder::class);
     }
 
-    function it_uses_the_index_creator(FinderKeyCreator $finderKeyCreator, \DateTime $date)
+    function it_uses_the_index_creator(FinderKeyCreator $finderKeyCreator)
     {
         $lastDigits = '1234';
         $amount = 3434.00;
-        $finderKeyCreator->createIndex($date, $lastDigits, $amount)->shouldBeCalled();
+        $finderKeyCreator->createIndex($this->date, $lastDigits, $amount)->shouldBeCalled();
+        $finderKeyCreator->createIndex($this->datePlusOne, $lastDigits, $amount)->shouldBeCalled();
 
-        $this->findConsignment($date, $lastDigits, $amount)->shouldReturn(null);
+        $this->findConsignment($this->date, $lastDigits, $amount)->shouldReturn(null);
     }
 
-    function it_finds_the_consignment(\DateTime $date)
+    function it_finds_the_consignment()
     {
-        $this->findConsignment($date, $this->lastDigits, $this->amount)->shouldReturn($this->consignment);
+        $this->findConsignment($this->date, $this->lastDigits, $this->amount)->shouldReturn($this->consignment);
     }
 
-    function it_returns_null_when_not_found(FinderKeyCreator $finderKeyCreator, \DateTime $date)
+    function it_returns_null_when_not_found(FinderKeyCreator $finderKeyCreator)
     {
-        $finderKeyCreator->createIndex($date, '0000', $this->amount)->willReturn('another_index');
+        $finderKeyCreator->createIndex($this->date, '0000', $this->amount)->willReturn('another_index');
+        $finderKeyCreator->createIndex($this->datePlusOne, '0000', $this->amount)->willReturn('another_index');
 
-        $this->findConsignment($date, '0000', $this->amount)->shouldReturn(null);
+        $this->findConsignment($this->date, '0000', $this->amount)->shouldReturn(null);
     }
 }
